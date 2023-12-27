33dacd1b00b13b726fe30da6dd1448153dc4f788
kenasd <kenasd@example.com>
2023-12-27 14:34:08 +1100

    try it out push main

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import HelpArea from './HelpArea';
+import HelpArea from "./HelpArea";
 
 function MainContent() {
-  const [helpVisible, setHelpVisible] = useState(false);
+ const [helpVisible, setHelpVisible] = useState(false);
 
-  function toggleHelp() {
-    setHelpVisible((isVisible) => !isVisible);
-  }
+ function toggleHelp() {
+  setHelpVisible((isVisible) => !isVisible);
+ }
 
-  return (
-    <main>
-      <button onClick={toggleHelp}>{helpVisible ? 'Hide' : 'Show'} Help</button>
-      {helpVisible && <HelpArea />}
-    </main>
-  );
+ return (
+  <main>
+   <h3>Hello World</h3>
+   <button onClick={toggleHelp}>{helpVisible ? "Hide" : "Show"} Help</button>
+   {helpVisible && <HelpArea />}
+  </main>
+ );
 }
 
 export default MainContent;
